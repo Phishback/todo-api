f9c6d1714c6eb06d06d0c367b9a3a5a654843393
phishback <fishback.evan@gmail.com>
2018-01-12 12:40:27 -0500

    Intial AJAX call

---
+++ public/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $.getJSON('/api/todos')
+  .then(addTodos)
+});
+
+function addTodos(todos) {
+  // add todos to page
+  todos.forEach(function(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    $('.list').append(newTodo);
+  })
+}
