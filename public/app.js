a839efbc6cb3b4b3ef1d816d051ceaea538ff49c
phishback <fishback.evan@gmail.com>
2018-01-12 13:09:21 -0500

    Connecting form to API

--- public/app.js
+++ public/app.js
@@ -1,15 +1,39 @@
 $(document).ready(function() {
   $.getJSON('/api/todos')
   .then(addTodos)
+
+  $('#todoInput').keypress(function(event) {
+    if(event.which === 13) {
+      //create todo
+      createTodo();
+    }
+  });
 });
 
 function addTodos(todos) {
   // add todos to page
   todos.forEach(function(todo) {
-    var newTodo = $('<li class="task">' + todo.name + '</li>');
-    if(todo.completed) {
-      newTodo.addClass("done");
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
   });
+}
+// add single todo
+function addTodo(todo) {
+  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  if(todo.completed) {
+    newTodo.addClass("done");
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  // send POST req to create todo
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', {name: usrInput})
+  .then(function(newTodo) {
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
 }
