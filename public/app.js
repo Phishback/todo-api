20e520bc8160cc3189d5134467858eb23f0ff0ba
phishback <fishback.evan@gmail.com>
2018-01-13 09:43:21 -0500

    Toggle todo completion

--- public/app.js
+++ public/app.js
@@ -9,7 +9,12 @@ $(document).ready(function() {
     }
   });
 
-  $('.list').on('click', 'span', function() {
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  })
+
+  $('.list').on('click', 'span', function(e) {
+    e.stopPropagation();
     removeTodo($(this).parent());
   });
 
@@ -25,6 +30,7 @@ function addTodos(todos) {
 function addTodo(todo) {
   var newTodo = $('<li class="task">' + todo.name + '<span>X</span</li>');
   newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed)
   if(todo.completed) {
     newTodo.addClass("done");
   }
@@ -56,5 +62,20 @@ function removeTodo(todo) {
   })
   .catch(function(err){
     console.log(err);
+  })
+}
+
+function updateTodo(todo) {
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo) {
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
   })
 }
